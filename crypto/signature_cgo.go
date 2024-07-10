e9e5882dfdbbcae1348c1925ae693c8f3bc7a337
pdrobnjak <drobnjakpavle95@gmail.com>
2024-07-10 06:58:49 +0200

    Remove duplicate symbols

--- crypto/signature_cgo.go
+++ crypto/signature_cgo.go
@@ -25,7 +25,7 @@ import (
 	"fmt"
 
 	"github.com/tenderly/mantle-op-geth/common/math"
-	"github.com/tenderly/mantle-op-geth/crypto/secp256k1"
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 )
 
 // Ecrecover returns the uncompressed public key that created the given signature.
